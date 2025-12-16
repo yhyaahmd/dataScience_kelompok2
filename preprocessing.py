4ad938b0388dd13f93e248345ba5e899b1f56b82
Yahya Ahmad <106219715+yhyaahmd@users.noreply.github.com>
2025-12-16 20:49:26 +0700

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -4,16 +4,6 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 
-st.markdown("""
-<style>
-@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap');
-
-html, body, [class*="css"]  {
-    font-family: 'Poppins', sans-serif;
-}
-</style>
-""", unsafe_allow_html=True)
-
 # ===============================
 # 1. MISSING VALUE
 # ===============================
